fe454aecf03634004a0b62aab2fd9e86765b2c36
joaquinboto <joaquin.boto@hotmail.com>
2022-03-23 15:32:52 -0300

    navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+const navBar = () => {
+
+    return (
+        <>
+        <div className="divNav">
+            <h1 className="h1Logo">Logo</h1>
+            <nav className="navE">
+            <ul className="lista">
+                <li className="listadoItem">Inicio</li>
+                <li className="listadoItem">E-SHOP</li>
+                <li className="listadoItem">Preguntas frecuentes</li>
+                <li className="listadoItem">Contacto</li>
+            </ul>
+            </nav>
+          </div>
+        </>
+      );
+
+}
+
+export default navBar
