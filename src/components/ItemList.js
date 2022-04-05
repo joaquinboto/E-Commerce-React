80b5170139616b2d3562a7820955ab3e8fa28954
joaquinboto <joaquin.boto@hotmail.com>
2022-04-05 19:34:15 -0300

    termine desafio

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -7,6 +7,7 @@ export default function ItemList ({productos}) {
         <>
         <div className="grilla">
             {productos.map(producto => <Item key={producto.id} {...producto}></Item>)}
+            
         </div>
         </>
     )
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,17 +2,30 @@ import ItemCount from "./ItemCount"
 import ItemList from './ItemList'
 import {autoFech} from '../products'
 import React, {useState , useEffect} from 'react'
+import Lottie from "lottie-react";
+import loading from '../assets/loading.json'
+
 
 
 const Container = () => {
-
+    
+    const load = {
+        animationData : loading,
+        autoplay: true,
+        loop: true,
+        style: {
+            width: '20%',
+        }
+    }
     //CAMBIANDO ESTADO DE LOS PRODUCTOS
     const [producto , setProducto] = useState([])
+    const [load2 , setLoad] = useState(<div className="row d-flex justify-content-center"><Lottie {...load}/></div>)
     //MONTANDO INFORMACION AL DOM DE LA PROMESA
     useEffect(() => {
 
         autoFech()
         .then((result) => setProducto(result))
+        .then( () => setLoad('Productos:')) 
         .catch((err) => console.error(err))
     },[])
 
@@ -25,7 +38,7 @@ const Container = () => {
         <>
             <div className="">
                 <div className="container__Titulo row">
-                    <h1 className="">Productos</h1>
+                    <h1 className="">{load2}</h1>
                     <div>
                          <ItemList productos={producto}/>
                         <ItemCount stock='5' initial='1' onAdd={add}/>
