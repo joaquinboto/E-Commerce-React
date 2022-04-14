ef04c0e13c2c0633bcb1557af226d6ea236c3338
joaquinboto <joaquin.boto@hotmail.com>
2022-04-14 14:14:23 -0300

    noentiendoporquenofiltra

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -10,20 +10,14 @@ import { useParams } from 'react-router-dom'
 const ItemDetailContainer = () => {
 
     const [productos , setProducts] = useState({})
-    const ItemId = useParams()
-
+    const {ItemId} = useParams()
 
     useEffect(() => {
 
-        if (ItemId === undefined) {
-            autoFech(products.find(product => product.id === 'Destacado'))
-            .then((result) => setProducts(result))
-            .catch((err) => console.error(err))
-        }else {
             autoFech(products.find(product => product.id === ItemId))
             .then((result) => setProducts(result))
             .catch((err) => console.error(err))
-        }
+        
     },[ItemId])
     
     return (
