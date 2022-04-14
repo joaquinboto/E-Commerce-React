c2c66366dc447da43bbbb05bed1156d7800d23b3
joaquinboto <joaquin.boto@hotmail.com>
2022-04-14 19:54:56 -0300

    casi

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -22,7 +22,6 @@ export const Row = styled.div`
    flex-direction: column;
    flex-wrap: wrap;
    margin: auto;
-   {}
 `
 
 export const Form = styled.div`
@@ -32,6 +31,21 @@ export const Form = styled.div`
     justify-content: center;
     flex-direction: column;
     margin: 0 20px;
+`
+
+export const Tbody = styled.tbody`
+display: flex;
+width: 295px;
+justify-content: space-around;
+align-items: center;
+`
+
+export const Table = styled.table`
+height: 200px;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+justify-content: space-evenly;
 `
 
 export const ItemDetail = ({productos}) => {
@@ -44,22 +58,22 @@ export const ItemDetail = ({productos}) => {
             <img className="imgDetail" alt="imagenProduct" src={productos.imagen}></img>
             </Row>
             <div>
-                <nav><Link className="navLinkDetail" to="/">Inicio</Link><span> / </span><Link className="navLinkDetail" to={`/category/${productos.path}`}>{productos.nombre}</Link></nav>
+                <nav><Link className="navLinkDetail" to="/">Inicio</Link><span> / </span><Link className="navLinkDetail" to={`/category/${productos.path}`}>{productos.path}</Link></nav>
                 <Form>
                 <h1 className="titleDetail" >{productos.nombre}</h1>
                 <p className="priceDetail">Precio: ${productos.precio}</p>
-                <p className="stockDetail">Stock:{productos.stock}</p>
+                <p className="stockDetail">Stock: {productos.stock}</p>
                 <div>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, est, perferendis porro enim error dolore quis quae voluptates dignissimos voluptate tenetur officia maiores cupiditate eveniet natus repudiandae sit illum adipisci.</p>
                 </div>
                 <form action="">
-                    <table>
-                        <tbody>
+                    <Table>
+                        <Tbody>
                             <th>
                                <label htmlFor="">Colores</label>
                             </th>
                             <td>
-                                <select name="" id="">
+                                <select name="" id="options">
                                     <option value="">Elije tu color</option>
                                     <option value="">Rojo</option>
                                     <option value="">Azul</option>
@@ -67,13 +81,13 @@ export const ItemDetail = ({productos}) => {
                                     <option value="">Amarillo</option>
                                 </select>
                             </td>
-                        </tbody>
-                        <tbody>
+                        </Tbody>
+                        <Tbody>
                             <th>
                                 <label htmlFor="">Tallas</label>
                             </th>
                             <td>
-                                <select name="" id="">
+                                <select name="" id="options">
                                     <option value="">Elije tu talla</option>
                                     <option value="">S</option>
                                     <option value="">M</option>
@@ -81,8 +95,8 @@ export const ItemDetail = ({productos}) => {
                                     <option value="">XL</option>
                                 </select>
                             </td>
-                        </tbody>
-                    </table>
+                        </Tbody>
+                    </Table>
                 </form>
                     <ItemCount stock={productos.stock}></ItemCount>
                 </Form>
